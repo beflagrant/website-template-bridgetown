90933387769c9565d976147427b980c7220e76f3
bigtiger <bigtiger@users.noreply.github.com>
2024-11-29 18:44:28 +0000

    Deploy preview for PR 5 🛫

---
+++ preview/pr-5/_bridgetown/static/index.AZ6QTIUE.js
@@ -0,0 +1,2 @@
+(()=>{console.info("Bridgetown is loaded!");})();
+//# sourceMappingURL=/website-template-bridgetown/preview/pr-5/_bridgetown/static/index.AZ6QTIUE.js.map
--- preview/pr-5/_bridgetown/static/index.TL6HAU3E.js
+++
@@ -1,2 +0,0 @@
-(()=>{console.info("Bridgetown is loaded!");})();
-//# sourceMappingURL=/preview/pr-5/_bridgetown/static/index.TL6HAU3E.js.map
